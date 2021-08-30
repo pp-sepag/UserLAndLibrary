4353e9ce6e543715ca0ea7265209e3a00fa27cb4
Corbin Champion <corbinlc@gmail.com>
2021-08-29 22:17:55 -0700

    handle if no speech  recorded and add some logging

--- app/src/main/java/tech/ula/RecordSpeechActivity.kt
+++ app/src/main/java/tech/ula/RecordSpeechActivity.kt
@@ -6,6 +6,7 @@ import android.os.Environment
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
 import java.io.IOException
@@ -22,6 +23,8 @@ class RecordSpeechActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.record_speech_activity)
 
+        Log.d("RecordSpeech", "onCreate")
+
         if ((intent != null) && intent?.type.equals("record_speech"))
             dispatchRecordSpeechIntent()
     }
@@ -37,6 +40,7 @@ class RecordSpeechActivity : AppCompatActivity() {
     }
 
     fun dispatchRecordSpeechIntent() {
+        Log.d("RecordSpeech", "dispatchRecordSpeechIntent")
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.packageName)
@@ -44,6 +48,7 @@ class RecordSpeechActivity : AppCompatActivity() {
         val recognizer: SpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this.applicationContext)
         val listener: RecognitionListener = object : RecognitionListener {
             override fun onResults(results: Bundle) {
+                Log.d("RecordSpeech", "onResults")
                 val voiceResults = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
                 var voiceResult = ""
                 if (voiceResults == null) {
@@ -57,6 +62,7 @@ class RecordSpeechActivity : AppCompatActivity() {
 
             override fun onReadyForSpeech(params: Bundle?) {
                 // TODO Auto-generated method stub
+                Log.d("RecordSpeech", "onReadyForSpeech")
             }
 
             /**
@@ -73,31 +79,42 @@ class RecordSpeechActivity : AppCompatActivity() {
              * @param error code is defined in SpeechRecognizer
              */
             override fun onError(error: Int) {
-                sendResult(-1)
+                Log.d("RecordSpeech", "onError")
+                if ((error == 7) || (error == 6)) {
+                    sendResult(1)
+                } else {
+                    sendResult(-1)
+                }
             }
 
             override fun onBeginningOfSpeech() {
                 // TODO Auto-generated method stub
+                Log.d("RecordSpeech", "onBeginningOfSpeech")
             }
 
             override fun onBufferReceived(buffer: ByteArray?) {
                 // TODO Auto-generated method stub
+                Log.d("RecordSpeech", "onBufferReceived")
             }
 
             override fun onEndOfSpeech() {
                 // TODO Auto-generated method stub
+                Log.d("RecordSpeech", "onEndOfSpeech")
             }
 
             override fun onEvent(eventType: Int, params: Bundle?) {
                 // TODO Auto-generated method stub
+                Log.d("RecordSpeech", "onEvent")
             }
 
             override fun onPartialResults(partialResults: Bundle?) {
                 // TODO Auto-generated method stub
+                Log.d("RecordSpeech", "onPartialResults")
             }
 
             override fun onRmsChanged(rmsdB: Float) {
                 // TODO Auto-generated method stub
+                Log.d("RecordSpeech", "onRmsChanged")
             }
         }
         recognizer.setRecognitionListener(listener)
